9cc936dec75efa61ab32003d8254993f2c678692
Jamila654 <136564835+Jamila654@users.noreply.github.com>
2024-03-27 21:00:46 +0500

    Add files via upload

---
+++ 78.Function Expressions vs. Function Declarations/main.js
@@ -0,0 +1,9 @@
+"use strict";
+function squareNumber(num1) {
+    return num1 * num1;
+}
+const square = function (number) {
+    return number * number;
+};
+console.log(squareNumber(3));
+console.log(square(2));
---
+++ 78.Function Expressions vs. Function Declarations/main.ts
@@ -0,0 +1,9 @@
+function squareNumber(num1:number){
+    return num1 * num1
+}
+
+const square = function(number: number){
+    return number * number
+}
+console.log(squareNumber(3))
+console.log(square(2))
