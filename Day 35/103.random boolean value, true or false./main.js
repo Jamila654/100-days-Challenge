49e7be63547373c70b16a862fe28910476983464
Jamila654 <136564835+Jamila654@users.noreply.github.com>
2024-04-06 00:45:14 +0500

    Add files via upload

---
+++ Day 35/103.random boolean value, true or false./main.js
@@ -0,0 +1,5 @@
+"use strict";
+function check(num) {
+    return num > 5;
+}
+console.log(check(10));
---
+++ Day 35/103.random boolean value, true or false./main.ts
@@ -0,0 +1,5 @@
+function check(num:number):boolean{
+    return num > 5
+}
+
+console.log(check(10))
